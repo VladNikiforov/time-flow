1400cdb4f2adcab44c1b73c819253e7775cecf94
Vlad Nikiforov <vlad40nikiforov@gmail.com>
2025-01-01 17:41:04 +0200

    commit

--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
 document.getElementById('main-page').addEventListener('click', () => {
-    chrome.tabs.create({ url: chrome.runtime.getURL('public/index.html') });
-});
+  chrome.tabs.create({ url: chrome.runtime.getURL('/public/index.html') })
+})
