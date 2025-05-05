61d21698026f6c8cdc397c0b6e4820ee9bccef2d
Vlad Nikiforov <vlad40nikiforov@gmail.com>
2025-05-05 12:14:17 +0300

    Improved dev flow

--- vite.config.ts
+++ vite.config.ts
@@ -7,38 +7,17 @@ const isChrome = process.env.BROWSER === 'chrome'
 const staticCopyPlugin = () => {
   return viteStaticCopy({
     targets: [
-      {
-        src: 'src/assets/favicon/**/*',
-        dest: 'assets/favicon',
-      },
-      {
-        src: ['src/assets/*.png', 'src/assets/*.svg'],
-        dest: 'assets',
-      },
-      {
-        src: 'src/global/*',
-        dest: 'global',
-      },
+      { src: 'src/assets/favicon/**/*', dest: 'assets/favicon' },
+      { src: ['src/assets/*.png', 'src/assets/*.svg'], dest: 'assets' },
+      { src: 'src/global/*', dest: 'global' },
       {
         src: resolve(__dirname, `src/manifest/${isChrome ? 'chrome.json' : 'firefox.json'}`),
         dest: './',
         rename: 'manifest.json',
       },
-      {
-        src: 'src/popup/popup.html',
-        dest: 'popup',
-        rename: 'popup.html',
-      },
-      {
-        src: 'src/public/index.html',
-        dest: 'public',
-        rename: 'index.html',
-      },
-      {
-        src: 'src/public/style.css',
-        dest: 'public',
-        rename: 'style.css',
-      },
+      { src: 'src/popup/popup.html', dest: 'popup', rename: 'popup.html' },
+      { src: 'src/public/index.html', dest: 'public', rename: 'index.html' },
+      { src: 'src/public/style.css', dest: 'public', rename: 'style.css' },
     ],
     watch: {
       reloadPageOnChange: true,
@@ -46,31 +25,37 @@ const staticCopyPlugin = () => {
   })
 }
 
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        background: resolve(__dirname, 'src/background.ts'),
-        script: resolve(__dirname, 'src/public/script.ts'),
-        popup: resolve(__dirname, 'src/popup/popup.ts'),
-      },
-      output: {
-        entryFileNames: (chunk) => {
-          const map: any = {
-            background: 'background.js',
-            script: 'public/script.js',
-            popup: 'popup/popup.js',
-          }
-          return map[chunk.name]
+export default defineConfig(({ mode }): any => {
+  const isDev = mode === 'development'
+
+  return {
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          background: resolve(__dirname, 'src/background.ts'),
+          script: resolve(__dirname, 'src/public/script.ts'),
+          popup: resolve(__dirname, 'src/popup/popup.ts'),
         },
-        assetFileNames: 'assets/[name].[ext]',
-        format: 'esm',
+        output: {
+          entryFileNames: (chunk: any) => {
+            const map: any = {
+              background: 'background.js',
+              script: 'public/script.js',
+              popup: 'popup/popup.js',
+            }
+            return map[chunk.name]
+          },
+          assetFileNames: 'assets/[name].[ext]',
+          format: 'esm',
+        },
       },
+      sourcemap: true,
+      emptyOutDir: true,
+      target: 'esnext',
+      minify: !isDev,
+      watch: isDev,
     },
-    sourcemap: true,
-    emptyOutDir: true,
-    target: 'esnext',
-  },
-  plugins: [staticCopyPlugin()],
+    plugins: [staticCopyPlugin()],
+  }
 })
